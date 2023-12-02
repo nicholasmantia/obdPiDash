501affaa3e23297c9c89652abfc08271c28fd802
Charlie Howard <charlie@howardgb.com>
2023-12-02 20:09:26 +0000

    Update 1.4.3

--- obdPiUpdate.sh
+++ obdPiUpdate.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 cd /home/pi/obdPiDash
-if [[ `git status --porcelain` ]]; then
-  sudo systemctl stop obdPiDash.service
-  git pull
-  sudo systemctl start obdPiDash.service
-else
-  fi
+git pull
 cd
-fi
+sudo systemctl restart obdPiDash.service
+exit
