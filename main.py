0e6b73abc67e2c24a67f8dfad8c39374f922e8f0
Nick Mantia <nmantia@equip.health>
2024-12-01 10:50:09 -0600

    Update main.py

--- main.py
+++ main.py
@@ -793,7 +793,7 @@ class MainApp(App):
         if OBD.enable.STFT and -16 <= int(round(self.STFT/OBD.gauge.persegment.STFT)) <= 16:
             self.STFT_Image = str('data/gauges/split/s2k_' + (str(int(round(self.STFT/OBD.gauge.persegment.STFT))))+'.png')
         if OBD.enable.LTFT and -8 <= int(round(self.LTFT/OBD.gauge.persegment.LTFT)) <= 8:
-            self.LTFT_Image = str('data/gauges/temp_normal/Coolant_' + (str(int(round(self.LTFT/OBD.gauge.persegment.LTFT))))+'.png')
+            self.LTFT_Image = str('data/gauges/split_temp_normal/Coolant_' + (str(int(round(self.LTFT/OBD.gauge.persegment.LTFT))))+'.png')
         if OBD.enable.ThrottlePos and 0 <= int(round(self.ThrottlePos/OBD.gauge.persegment.ThrottlePos)) <= 32:
             self.ThrottlePos_Image = str('data/gauges/normal/s2k_'+(str(int(round(self.ThrottlePos/OBD.gauge.persegment.ThrottlePos))))+'.png')
         if OBD.enable.Load and 0 <= int(round(self.Load/OBD.gauge.persegment.Load)) <= 32:
